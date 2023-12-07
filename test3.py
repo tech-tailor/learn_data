a5eeb0ce35a5e72a99d26f3f7c9bbbc4689767b3
Akin <ogundareakinnyi8@gmail.com>
2023-12-07 10:52:53 +0000

    update

--- test3.py
+++ test3.py
@@ -12,10 +12,10 @@ import time
 
 def get_teams():
     countries = {
-            #"Italy":"italy",
-            #"France":"france",
-            #"England":"england",
-            #"Denmark": "denmark",
+            "Italy":"italy",
+            "France":"france",
+            "England":"england",
+            "Denmark": "denmark",
             "Brasil": "brazil",
             "Belgium": "belgium",
             "Germany": "germany",
@@ -56,7 +56,7 @@ def get_teams():
         
         try:
             # Find the element you want to click (for example, a button with id='myButton')
-            element_to_click = WebDriverWait(driver, 10).until(
+            element_to_click = WebDriverWait(driver, 7).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, ".lmc__itemMore"))
             )
             ##print('more is going to be clicked')
@@ -225,6 +225,24 @@ def get_teams():
                                 
                                 teams_played_with = last_match['title'][16:].replace('\n', '').replace('b]', '').strip()
                                 last_played_with.append(teams_played_with)
+                              
+                            #get total league name and rank at a go
+                            all_ranks_and_names =[]
+                            no_of_all_teams = 0
+                            for league_rank_and_name in league_teams:
+                                no_of_all_teams += 1
+                                just_rank = int(league_rank_and_name.find('div', class_='tableCellRank').text.rstrip('.'))
+                                just_team_name = league_rank_and_name.find('a', class_="tableCellParticipant__name").text.strip()
+
+                                just_data ={
+                                    'just_rank': just_rank,
+                                    'just_team_name': just_team_name,
+                                }
+                                all_ranks_and_names.append(just_data)
+                                continue
+
+
+
                             
                             
 
@@ -235,7 +253,7 @@ def get_teams():
                             next_match[0]
                             #print(rank, team_name, matches_played, points, last_matches, last_played_with)
 
-                            match_data ={
+                            match_datas ={
                                 'rank': rank,
                                 'team_name': team_name,
                                 'matches_played': matches_played,
@@ -244,24 +262,59 @@ def get_teams():
                                 'next_match': next_match,
                                 'last_5_matches': last_5_matches
                             }
-
-                            
-                            #print(match_data)
+                            #print(match_datas)
                             #print('')
 
-                            if last_matches[:2] == ['L', 'L'] and rank <= 4:
-                                print(match_data)
+                            if last_matches[:1] == ['L'] and rank <= 11:
+                                #print(match_data)
+                                nextmatch_teams = next_match[0]
                                 print('')
-                                with open("league_results.txt", "a") as file:
-                                    file.write(str(match_data) + '\n')
-                           
+                                nextmatch_opponent = nextmatch_teams[:-10]   #remove date attached to the teams
+                                teams = nextmatch_opponent.split('-')
+                                print(teams)
+                                if team_name == teams[1].strip():
+                                    opponent = teams[0].strip()
+                                else:
+                                    opponent = teams[1].strip()
+                                
+                                
+                                
+                                
+                        
+                                
+                                #print(all_rank_and_name)
+                                #print(no_of_all_teams)
+                                
+                                print(f"nextmatch: {nextmatch_opponent}")
+                                print(f"team with advantage: {team_name}, rank: {rank}")
+                                #search for opponent rank
+                                opponent_rank = 0
+                                for all_rank_and_name in all_ranks_and_names:
+                                    #print(all_rank_and_name)
+                                    if all_rank_and_name['just_team_name'] == opponent:
+                                        opponent_rank += all_rank_and_name['just_rank']
+                                        break
+                                        
+                                    
+                                        
+                                
+                                #print(opponent_rank)
+                                if opponent_rank >= (no_of_all_teams - 5):
+                                    
+                                    print("team with opponenents in the bottom 5")
+                                    print(f" {team_name}-{rank} vs {opponent}-{opponent_rank}")
+                                
 
+                                #with open("league_results2.txt", "a") as file:
+                                    #file.write(str(match_data) + '\n')
+                    
+                           
                         league_details = league_country, 'total_league_teams:', total_league_teams
                         print(league_details)
                         print('')
                         print('')
-                        with open("league_results.txt", "a") as file:
-                            file.write(str(league_details) + '\n' + '\n')
+                        #with open("league_results2.txt", "a") as file:
+                            #file.write(str(league_details) + '\n' + '\n')
 
                     except Exception as e:
                         print('error1: ', e)
@@ -280,9 +333,8 @@ def get_teams():
         driver.quit()
 
 
-
 def main():
-    max_retries = 10
+    max_retries = 5
     retries = 0
 
     while retries < max_retries:
@@ -302,7 +354,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
-
-
-
---
+++ test4.py
@@ -0,0 +1,11 @@
+match = 'Brighton - Brentford06.12.2023'
+
+teams = match[:-10]
+team = teams.split()
+home_team = team[0]
+away_team = team[2]
+
+print(home_team)
+print(away_team)
+
+
